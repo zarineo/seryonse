81e0997da985df02bd74546d43526d8cfd038859
Zarina <tochilka007@mail.ru>
2023-12-07 23:13:53 +0300

    Отправка на почту.
    Настоящий бэк написал скрипт на php.

--- send.php
+++ send.php
@@ -1,21 +1,13 @@
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="email" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
-} else {
- //показываем форму
-
+if(isset($_POST['email'])) {
  $email = $_POST['email'];
  $email = htmlspecialchars($email);
  $email = urldecode($email);
  $email = trim($email);
- if (mail("tochilka007@mail.ru", "Заявка с сайта", E-mail: ".$email ,"From: xomya4ik@gmail.com \r\n")){
- echo "Сообщение успешно отправлено";
- } else {
- echo "При отправке сообщения возникли ошибки";
- }
+    
+ if (mail("tochilka007@mail.ru", "client", "E-mail: ".$email ,"From: xomya4ik@gmail.com \r\n")){
+    echo "Success";
+} else {
+    echo "fartal error!";
 }
-?>
+}
