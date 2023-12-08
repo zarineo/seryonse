280ea6fb2b0137851e903d0f7c61823f548902de
Zarina <tochilka007@mail.ru>
2023-12-08 21:28:08 +0300

    Надеюсь, что отступы уменьшила.
    Далмукълахь.

--- send.php
+++ send.php
@@ -1,13 +1,21 @@
 <?php
-if(isset($_POST['email'])) {
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['email'])){
+ ?> <form action="send.php" method="post">
+<input type="email" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form> <?php
+} else {
+ //показываем форму
+
  $email = $_POST['email'];
  $email = htmlspecialchars($email);
  $email = urldecode($email);
  $email = trim($email);
-    
- if (mail("tochilka007@mail.ru", "client", "E-mail: ".$email ,"From: xomya4ik@gmail.com \r\n")){
-    echo "Success";
-} else {
-    echo "fartal error!";
+ if (mail("tochilka007@mail.ru", "Заявка с сайта", E-mail: ".$email ,"From: xomya4ik@gmail.com \r\n")){
+ echo "Сообщение успешно отправлено";
+ } else {
+ echo "При отправке сообщения возникли ошибки";
+ }
 }
-}
+?>
