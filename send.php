2e073b5b2c1fc95601fc4450838be5e4dc499790
Zarina <tochilka007@mail.ru>
2023-12-11 22:54:23 +0300

    Форма

--- send.php
+++ send.php
@@ -1,21 +1,11 @@
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="email" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
-} else {
- //показываем форму
-
- $email = $_POST['email'];
- $email = htmlspecialchars($email);
- $email = urldecode($email);
- $email = trim($email);
- if (mail("tochilka007@mail.ru", "Заявка с сайта", E-mail: ".$email ,"From: xomya4ik@gmail.com \r\n")){
- echo "Сообщение успешно отправлено";
- } else {
- echo "При отправке сообщения возникли ошибки";
- }
-}
+    $email = $_POST['email'];
+    $to = "tochilka007@mail.ru"; 
+    $date = date ("d.m.Y"); 
+    $time = date ("h:i");
+    $from = $email;
+    $subject = "Р—Р°СЏРІРєР° c СЃР°Р№С‚Р° Seryonse";
+    $msg="
+    Email: $email";   
+  mail($to, $subject, $msg, "From: $to ");
 ?>
